dbb4d613918833ed4f58a38940234472dc14e558
tysker <oerteljoerg@gmail.com>
2020-07-29 18:36:18 +0200

    Updates

---
+++ precache-manifest.53668cc5cf08b090564ec05c98b2232e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02e0b533d48862db59eea6297c4955c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b4339dca4b9015c6c355",
+    "url": "/monster-rolodex/static/css/main.df42f742.chunk.css"
+  },
+  {
+    "revision": "c4d11fd425a1c893d7fd",
+    "url": "/monster-rolodex/static/js/2.a6404bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a6404bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4339dca4b9015c6c355",
+    "url": "/monster-rolodex/static/js/main.b7e294bf.chunk.js"
+  },
+  {
+    "revision": "33f8011fe69470eb33fe",
+    "url": "/monster-rolodex/static/js/runtime-main.c9f53522.js"
+  }
+]);
