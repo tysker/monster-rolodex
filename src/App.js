e8fa61ed60711be91521e7131208d2a1808c3eb5
tysker <oerteljoerg@gmail.com>
2020-07-30 15:00:14 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { SearchBox } from "./components/searchBox/search-box.component";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       monsters: [],
       searchField: "",
+      number: 0
     };
-
+    console.log("constructor");
     //this.handleChange = this.handleChange.bind(this);
   }
 
@@ -25,13 +26,34 @@ class App extends Component {
     this.setState({ searchField: e.target.value })
   }
 
+  handleBtn = (e) => {
+    this.setState( (prevState, prevProps) => {
+      console.log(prevState)
+      console.log(prevProps)
+      return {number: prevState.number + prevProps.increment}
+    }, () => console.log(this.state.number))
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //console.log('shouldComponentUpdate!', nextProps);
+    console.log("=============");
+    console.log(nextProps);
+    console.log(nextState);
+    console.log(this.props.increment);
+    console.log("=============");
+    return nextProps.number !== this.props.increment;
+  }
+
   render() {
+    console.log("render")
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     return (
       <div className="App">
+        <h3>{this.state.number}</h3>
+        <button onClick={this.handleBtn}>Increment</button>
         <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="search monsters..."
